e803dc7065cbcfb8cbc0931faf06daac519bf1f9
UsuallyChris <schoonc@gmail.com>
2019-05-08 16:54:12 -0700

    Added class based view and routing for home.html. Configured template dir in settings.py

--- django_book_2/django_book_2/settings.py
+++ django_book_2/django_book_2/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'django_book_2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
